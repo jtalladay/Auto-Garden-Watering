f1638f2a848b85a8636214202a4f3111c1bf7f5b
jtalladay <93738150+jtalladay@users.noreply.github.com>
2023-10-10 18:42:03 -0400

    Update GardenSensor.ino
    removed excess code pertaining to adafruit io connection

--- GardenSensor.ino
+++ GardenSensor.ino
@@ -1,13 +1,10 @@
 /*This program is designed to use the feather board to read temp and humidity 
- * using the DHT11 sensor, and read moisture using diy moisture sensor, and upload
- * this data to adafruit IO as well as displaying this information to an  
- * onboard LCD display. It also needs to trigger a solenoid water valve
+ * using the DHT11 sensor, and read moisture using diy moisture sensor, and save data to text file.
+ * It also needs to trigger a solenoid water valve
  * based on the moisture reading. 
  */
 
-//#include "config.h"
 #include <LiquidCrystal.h>
-#include <Adafruit_Sensor.h>
 #include <DHT.h>
 
 #define MOISTURE_PIN A0
@@ -20,11 +17,6 @@ int solenoidPin = 14;
 int current_moisture = 0;
 int tempPin = 0;
 
-// set up the 'soil_moisture' feed
-//AdafruitIO_Feed *soil_moisture = io.feed("soil_moisture");
-//AdafruitIO_Feed *Humidity = io.feed("Humidity");
-//AdafruitIO_Feed *Temperature = io.feed("Temperature");
-
 //                RS  E  D4 D5  D6  D7
 LiquidCrystal lcd(16, 2, 0, 15, 13, 12);
 DHT dht(DHTPIN, DHTTYPE);
@@ -34,67 +26,31 @@ void setup()
   lcd.begin(16, 2);
   dht.begin();
   pinMode(solenoidPin, OUTPUT);
-  
- // connect to io.adafruit.com
-  //lcd.print("Connecting to Adafruit IO");
-  //io.connect();
-  /*
-  // wait for a connection
-  while(io.status() < AIO_CONNECTED) {
-   lcd.print(".");
-    delay(1000);
-  }
-  
-   // we are connected
-  lcd.println();
-  lcd.println(io.statusText());
 
-  lcd.begin(16, 2);
 }
-*/
-}
+
 void loop(){
+
+  digitalWrite(solenoidPin, LOW);
 
   //start with a moisture check turning the water valve on if necassary
   current_moisture = analogRead(MOISTURE_PIN);
-  //digitalWrite(SOLENOID, LOW);
-
-  if (current_moisture < 280){
-     digitalWrite(solenoidPin, HIGH);  
+  
+  while (current_moisture < 280){
+    digitalWrite(solenoidPin, HIGH);  
     delay(7200);
-  }    else {
-    digitalWrite(solenoidPin, LOW);
-  }
-  
+  }   
 
-   // io.run(); is required for all sketches.
-  // it should always be present at the top of your loop
-  // function. it keeps the client connected to
-  // io.adafruit.com, and processes any incoming data.
-  //io.run();
-  
-  // grab the current state of the moisture sensor
-  // read humidity (h)
-  // read temperature (f)
   current_moisture = analogRead(MOISTURE_PIN);
-  float h = dht.readHumidity();
-  //uncomment for celsius
-  //float t = dht.readTemperature();
-  float f = dht.readTemperature(true);
-  
-  //send all values to adafruit io
-  // save the current state to the analog feed
-  //feed name -> save (variable name)
-  //soil_moisture->save(current_moisture);
-  //Humidity->save(h);
-  //Temperature->save(f);
+  float humidity = dht.readHumidity();
+  float temp = dht.readTemperature(true);
   
   // print out to lcd       
   lcd.print("T       H    ");
   lcd.setCursor(2, 0);
-  lcd.print(f);
+  lcd.print(temp);
   lcd.setCursor(10,0);
-  lcd.print(h);
+  lcd.print(humidity);
   
   // Display Moisture on second row
   int moistureReading = analogRead(current_moisture);
@@ -107,8 +63,3 @@ void loop(){
   delay(10000);
   
 }
-
-
-
-
-
